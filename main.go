33decf2629a391d1d9ecc0590df7ce93ff98ebd5
Mathieu Hays <5703113+mathieuhays@users.noreply.github.com>
2024-08-24 16:49:58 +0200

    expect url as argument

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+func run(args []string, out io.Writer) error {
+	if len(args) < 2 {
+		return errors.New("no website provided")
+	} else if len(args) > 2 {
+		return errors.New("too many arguments provided")
+	}
+
+	url := args[1]
+	if _, err := fmt.Fprintf(out, "starting crawl of: %s", url); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	err := run(os.Args, os.Stdout)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
